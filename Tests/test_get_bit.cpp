ded4f0198ef4a872f3d3bcde84e9de4776f08274
SlavaShagalov <slavikshagalov@gmail.com>
2022-10-20 15:37:34 +0300

    Test not good coverage

--- Tests/test_get_bit.cpp
+++ Tests/test_get_bit.cpp
@@ -2,20 +2,20 @@
 
 #include "bit_operation.hpp"
 
-TEST(GetBit, Bit0) {
-    uint32_t num = 25;
-    uint8_t index = 2;
-
-    bool value = getBit(num, index);
-
-    EXPECT_EQ(value, 0);
-}
-
-TEST(GetBit, Bit1) {
-    uint32_t num = 25;
-    uint8_t index = 3;
-
-    bool value = getBit(num, index);
-
-    EXPECT_EQ(value, 1);
-}
+//TEST(GetBit, Bit0) {
+//    uint32_t num = 25;
+//    uint8_t index = 2;
+//
+//    bool value = getBit(num, index);
+//
+//    EXPECT_EQ(value, 0);
+//}
+//
+//TEST(GetBit, Bit1) {
+//    uint32_t num = 25;
+//    uint8_t index = 3;
+//
+//    bool value = getBit(num, index);
+//
+//    EXPECT_EQ(value, 1);
+//}
